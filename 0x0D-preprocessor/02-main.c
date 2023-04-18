6ed4e215905ab1850c4e6062b3c8325a0992243a
sonnie12 <dorcusmwangi2005@gmail.com>
2023-04-18 02:19:14 -0700

    2. File name

---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints name of the file
+ * Description: print the name of the source file
+ * Return: returns a 0 when correct.
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
